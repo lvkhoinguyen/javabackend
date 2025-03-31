0f12a1319391e906864fcc8b471bf88d52743ab4
levokhoinguyen.it@gmail.com <levokhoinguyen.it@gmail.com>
2025-03-31 21:43:50 +0700

    Mô tả thay đổi

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,8 +6,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class HelloController {
+
+
     @GetMapping("/")
-        public String index() {
-        return "Hello World from LVKN!";
+    public String index() {
+        return "Hello World from update!";
     }
+
+    @GetMapping("/user")
+    public String userPage() {
+        return "Only User can see this!";
+    }
+
+    @GetMapping("/admin")
+    public String adminPage() {
+        return "Only Admin can see this!";
+    }
+
+
 }
