51af67b3fee5104c68a1bf17209d5b5193f62ecb
levokhoinguyen.it@gmail.com <levokhoinguyen.it@gmail.com>
2025-04-19 22:00:27 +0700

    19/04

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -8,24 +8,28 @@ import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-
-
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
-@Bean
-public ViewResolver viewResolver() {
-    final InternalResourceViewResolver bean = new InternalResourceViewResolver();
-    bean.setViewClass(JstlView.class);
-    bean.setPrefix("/WEB-INF/view/");
-    bean.setSuffix(".jsp");
-    return bean;
-}
+    @Bean
+    public ViewResolver viewResolver() {
+        final InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setViewClass(JstlView.class);
+        bean.setPrefix("/WEB-INF/view/");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
 
-@Override
-public void configureViewResolvers(ViewResolverRegistry registry) {
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.viewResolver(viewResolver());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/resources/css/");
     }
 }
