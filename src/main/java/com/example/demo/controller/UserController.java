c3cfe46b75c8c9507099285b83fe71d7870b3daa
levokhoinguyen.it@gmail.com <levokhoinguyen.it@gmail.com>
2025-04-20 21:17:44 +0700

    20/04/2025

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
+// import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.User;
 
 
 import com.example.demo.service.UserService;
@@ -36,10 +41,15 @@ public class UserController {
     @RequestMapping("/admin/user")
     public String getUserPage(Model model) {
         String test = this.userService.handleHello();
-        model.addAttribute("eric", test);
-        model.addAttribute("hoidanit", "helloeric");
-        
+        model.addAttribute("newUser", new User());
         return "admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model,@ModelAttribute("newUser") User hoidanit) {
+        System.out.println("run here "+ hoidanit);
+        return "hello";
+
     }
 
 }
