c3f73132d67d6bccbc1e7d587135521a72162573
levokhoinguyen.it@gmail.com <levokhoinguyen.it@gmail.com>
2025-04-28 22:32:12 +0700

    28/04/2025

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -14,4 +14,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User save(User eric);
     List<User> findByEmail(String email);
     List<User> findAll();
+    User findById(long id);
 }
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -28,6 +28,10 @@ public class UserService {
         return this.userRepository.findByEmail(email);
     }
 
+    public User getUserById(long id){
+        return this.userRepository.findById(id);
+    }
+
     public User handleSaveUser(User user) {
         User eric = this.userRepository.save(user);
         System.out.println(eric);
