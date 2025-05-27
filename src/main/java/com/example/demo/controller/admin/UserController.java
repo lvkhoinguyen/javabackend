135c100061f6e62fd79b4d097281ee8a11562bb8
levokhoinguyen.it@gmail.com <levokhoinguyen.it@gmail.com>
2025-05-27 11:32:57 +0700

    27/05

--- src/main/java/com/example/demo/controller/admin/UserController.java
+++ src/main/java/com/example/demo/controller/admin/UserController.java
@@ -1,11 +1,10 @@
 package com.example.demo.controller.admin;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.List;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -17,6 +16,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.domain.User;
 import com.example.demo.repository.UserRepository;
+import com.example.demo.service.UploadService;
 import com.example.demo.service.UserService;
 
 import jakarta.servlet.ServletContext;
@@ -31,11 +31,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class UserController {
 
     private final UserService userService;
-    private final ServletContext servletContext;
+    private final UploadService uploadService;
+    private final PasswordEncoder passwordEncoder ;
 
-    public UserController(UserService userService, ServletContext servletContext) {
+    public UserController(UserService userService, UploadService uploadService,
+            ServletContext servletContext,
+            PasswordEncoder passwordEncoder) {
         this.userService = userService;
-        this.servletContext = servletContext;
+        this.uploadService = uploadService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @RequestMapping("/")
@@ -73,30 +77,14 @@ public class UserController {
     @PostMapping("/admin/user/create")
     public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit,
             @RequestParam("hoidanitFile") MultipartFile file) {
-
-        try {
-            byte[] bytes = file.getBytes();
-
-            String rootPath = this.servletContext.getRealPath("/resources/images");
-
-            File dir = new File(rootPath + File.separator + "avatar");
-            if (!dir.exists())
-                dir.mkdirs();
-
-            // Create the file on server
-            File serverFile = new File(dir.getAbsolutePath() + File.separator +
-                    +System.currentTimeMillis() + "-" + file.getOriginalFilename());
-
-            BufferedOutputStream stream = new BufferedOutputStream(
-                    new FileOutputStream(serverFile));
-            stream.write(bytes);
-            stream.close();
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        System.out.println("run here " + hoidanit);
-        // this.userService.handleSaveUser(hoidanit);
+        String avatar = this.uploadService.handSaveUploadFile(file, "avatar");
+        String hashPassWord = this.passwordEncoder.encode(hoidanit.getPassword());
+        
+        hoidanit.setAvatar(avatar);
+        hoidanit.setPassword(hashPassWord);
+        hoidanit.setRole(this.userService.getRoleByName(hoidanit.getRole().getName()));
+        //saveRole
+        
         return "redirect:/admin/user";
 
     }
---
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.domain.Role;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name); // Tìm kiếm theo thuộc tính name trong Role
+}
+
